f22fb1d1a1f08afce670a09bc9ceddc70248cbde
AlonBarak-dev <alonbarak3110@gmail.com>
2022-05-10 00:59:23 +0300

    update malloc and free

--- stack.hpp
+++ stack.hpp
@@ -13,5 +13,9 @@ void push(pnode *head, char data[1024]); // push -> receives head of stack (doub
 void pop(pnode *head);  // pop -> receives head of stack (double pointer)
 char* top(pnode *head);  // top -> receives head of stack (pointer)
 
+void* _malloc(size_t size);
+void* _calloc(size_t size);
+void _free(void *address);
+
 #endif
 
